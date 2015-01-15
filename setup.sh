5c252b9247d00053ebb7f326fe8a4906aa474c29
SkaveRat <skaverat@funlynx.de>
2015-01-15 20:06:33 +0100

    fixing setup file
    pwd works better for dir detection. realpath not installed on every machine
    
    adding bindata lib via documented path

--- setup.sh
+++ setup.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-CWD=`realpath $(dirname $0)`
-BINDATA_PKG="github.com/jteeuwen/go-bindata"
+CWD=`pwd`
+BINDATA_PKG="github.com/jteeuwen/go-bindata/..."
 
 if [ ! -n "${GOPATH}" ]; then
     echo "GOPATH not set"
     exit 255
 fi
 
-echo " # Get go-bindata"
-go get "$BINDATA_PKG"
-echo " # Install go-bindata"
-go install "$BINDATA_PKG"
+echo " # Get and install go-bindata"
+go get -u "$BINDATA_PKG"
 
 echo " # Generate files"
 exec $GOPATH/bin/go-bindata -pkg="storage" -o="${CWD}/storage/schemas.go" -prefix="${CWD}/storage" "${CWD}/storage/schemas/..."
